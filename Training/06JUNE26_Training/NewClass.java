965d3c17d6cb4bde54d4bd772a302c8bb06c9c61
Aditya Srivastava <aditya.srivastava1409@gmail.com>
2026-06-12 22:44:53 +0530

    Delete Training directory

--- Training/06JUNE26_Training/NewClass.java
+++
@@ -1,8 +0,0 @@
-public class NewClass {
-     public static void main(String[] args) {
-           int x = 2;
-           int y = --x  + x++ + x--;
-           System.out.println(y);
-           System.out.println(x);
-    }
-}
--- Training/10JUNE26_Training/Solution58.java
+++
@@ -1,9 +0,0 @@
-public class Solution58 {
-    public static void main(String[] args) {
-        System.out.println(lengthOfLastWord("Hello World"));
-    }
-    public static int lengthOfLastWord(String s) {
-        s = s.trim();
-        return s.length() - s.lastIndexOf(' ') - 1;
-    }
-}
