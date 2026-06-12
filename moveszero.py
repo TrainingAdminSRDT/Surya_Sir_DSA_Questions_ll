96cfb9c68687f5b636a3a2713389b9be1d36954b
Hariom-abc <hariomshukla2022@gmail.com>
2026-06-12 23:05:57 +0530

    Add files via upload

---
+++ moveszero.py
@@ -0,0 +1,7 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        count = nums.count(0)
+
+        for i in range(count):
+            nums.remove(0)
+            nums.append(0)
