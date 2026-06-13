293f01e34294287156736306fb9faa61d275c49a
Harsh647 <70998256+Harsh647@users.noreply.github.com>
2026-06-13 14:44:50 +0530

    Add files via upload

---
+++ removeduplicate.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        int j = 1;
+        if(n == 0){
+            return 0;
+        }
+        for(int i = 1 ; i < n ; i++){
+            if(nums[i]!= nums[i-1]){
+                nums[j] = nums[i];
+                j++;
+            }
+        }return j;
+    }
+} {
+    
+}
--- sortedSquares.java
+++ sortedSquares.java
@@ -1,37 +1,10 @@
-import java.util.Arrays;
- class Solution  {
+class Solution {
     public int[] sortedSquares(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[n];
-        int left = 0;
-        int right = n - 1;
-        int index = n - 1; 
-
-        while (left <= right) {
-            int leftSquare = nums[left] * nums[left];
-            int rightSquare = nums[right] * nums[right];
-
-            if (leftSquare > rightSquare) {
-                result[index] = leftSquare;
-                left++;
-            } else {
-                result[index] = rightSquare;
-                right--;
-            }
-            index--;
+        int[] ans;
+        for(int i = 0 ; i < nums.length;i++){
+            nums[i] = nums[i] * nums[i];
         }
-        
-        return result;
-    }
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        int[] nums1 = {-4, -1, 0, 3, 10};
-        int[] result1 = sol.sortedSquares(nums1);
-        System.out.println("Input: " + Arrays.toString(nums1));
-        System.out.println("Output: " + Arrays.toString(result1) + "\n");
-        int[] nums2 = {-7, -3, 2, 3, 11};
-        int[] result2 = sol.sortedSquares(nums2);
-        System.out.println("Input: " + Arrays.toString(nums2));
-        System.out.println("Output: " + Arrays.toString(result2));
+        java.util.Arrays.sort(nums);
+        return nums;       
     }
 }
