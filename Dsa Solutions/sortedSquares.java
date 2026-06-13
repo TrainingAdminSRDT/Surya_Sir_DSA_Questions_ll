33fbc037b80dfd07e01004d5de3e72f2fcd62151
Harsh647 <70998256+Harsh647@users.noreply.github.com>
2026-06-13 14:52:49 +0530

    Add files via upload

---
+++ Dsa Solutions/sortedSquares.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int[] ans;
+        for(int i = 0 ; i < nums.length;i++){
+            nums[i] = nums[i] * nums[i];
+        }
+        java.util.Arrays.sort(nums);
+        return nums;       
+    }
+}
