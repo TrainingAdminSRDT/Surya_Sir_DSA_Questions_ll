392b03459735d4532da43f3d139147c7b0f174dc
alokmishra0905 <alokmishra0905@gmail.com>
2026-06-13 15:07:42 +0530

    Add files via upload

---
+++ anagram.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+//Problem 242
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        
+        vector<int> freq(26, 0);
+        for(int i = 0; i < s.length(); i++){
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+        }
+        for(int i = 0; i< 26; i++){
+            if(freq[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
