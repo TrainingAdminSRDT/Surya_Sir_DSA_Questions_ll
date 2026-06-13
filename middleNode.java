00d6ca3d29664dddd8d259c76b2ea5bf16541d92
princeyadav56 <akanksha884067@gmail.com>
2026-06-13 14:06:19 +0530

    Add files via upload

--- middleNode.java
+++ middleNode.java
@@ -1,28 +1,20 @@
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
-public class middleNode {
-    public ListNode findMiddle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;        
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }   
-        return slow;
-    }
-    public static void main(String[] args) {
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
-        middleNode solver = new middleNode();
-        ListNode middle = solver.findMiddle(head);
-        System.out.println("Middle Node Value: " + middle.val);
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head , slow = head ;
+        while(fast.next != null && fast.next.next != null){
+            fast = fast.next.next ;
+            slow = slow.next ;
+        }
+        return fast.next != null ? slow.next : slow ;
     }
 }
--- removeElement.java
+++ removeElement.java
@@ -1,30 +1,12 @@
-import java.util.Arrays;
-
 class Solution {
-    // LeetCode ka main logic function
     public int removeElement(int[] nums, int val) {
-        int k = 0; 
+        int index = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-                nums[k] = nums[i]; 
-                k++; 
+                nums[index] = nums[i];
+                index++;
             }
         }
-        return k; 
-    }
-    public static void main(String[] args) {
-        Solution solver = new Solution();
-        int[] nums = {3, 2, 2, 3};
-        int val = 3;
-        System.out.println("Original Array: " + Arrays.toString(nums));
-        System.out.println("Value to remove: " + val);
-        int k = solver.removeElement(nums, val);
-        System.out.println("\n--- Result ---");
-        System.out.println("New length (k): " + k);
-        System.out.print("Modified Array (first " + k + " elements): [");
-        for (int i = 0; i < k; i++) {
-            System.out.print(nums[i] + (i < k - 1 ? ", " : ""));
-        }
-        System.out.println("]");
+        return index;
     }
 }
